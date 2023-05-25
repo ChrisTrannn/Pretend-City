726ef70cccbc0e4f6829c3f290e4bbb63fa8f1a3
Chris Tran <christran1773@gmail.com>
2023-05-24 20:49:55 -0700

    fixed wording

--- src/components/home/Modal2.js
+++ src/components/home/Modal2.js
@@ -6,15 +6,16 @@ const Modal2 = ({closeModal2}) => {
         <div className='modal'>
             <div className='body'>
                 <div className='text'>
-                    Team Tap2Play is a UCI based team where our 
+                    Tap2Play is a UCI based team where our 
                     passion for merging technology and education led us
                     to win the competition at UCI's ANTrepreneur Center
-                    for our project with the Pretend City Children Museum.
+                    for our project with the Pretend City.
                     We were tasked with integrating technology throughout
                     the museum to enhance the learning experience for adult visitors.
                     We are proud to present Tap2Play, a mobile application that
                     allows parents to NFC tap their phones on signs throughout the museum.
-                    The team consists of: Chris Tran, Joseph Saada, Rosalie Chen, and Isabella Cao.
+                    The team consists of 3 developers: Chris Tran, Joseph Saada, and Aaliyah Mendoza. 
+                    And 2 project coodinators: Rosalie Chen, and Isabella Cao.
                 </div>
             </div>
             <button className='modalClose' onClick={() => closeModal2(false)}> Close </button>
